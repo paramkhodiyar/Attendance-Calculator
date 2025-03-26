cc9d108309fe9d3a1505062234457e4a73718303
Param Khodiyar <paramkhodiya1008@gmail.com>
2025-03-26 16:31:08 +0530

    Added basic Calculator functionalty in the page and fixed the navigation to page issue..

--- src/components/calcpage/calcpage.jsx
+++ src/components/calcpage/calcpage.jsx
@@ -1,8 +1,55 @@
-import React from "react";
+import React, { useState } from "react";
 import "./calcpage.css";
 
 function CalcPage({ darkMode }) {
-    return <h1 style={{ color: darkMode ? "white" : "black" }}>Calculator Page</h1>;
-  }
-  
+    const [percentage, setPercentage] = useState(null);
+    const [classesTotal, setClassesTotal] = useState(null);
+    const [classesAttended, setClassesAttended] = useState(null);
+
+    function handleSubmit(e) {  
+        e.preventDefault();
+        const classesTotal = parseFloat(e.target[0].value);
+        const classesAttended = parseFloat(e.target[1].value);
+        const classesSemester = parseFloat(e.target[2].value);
+        setClassesTotal(classesTotal);
+        setClassesAttended(classesAttended);
+
+        if (classesTotal > 0) {
+            const calculatedPercentage = (classesAttended / classesTotal) * 100;
+            setPercentage(calculatedPercentage.toFixed(2));
+        } else {
+            setPercentage("Invalid input");
+        }
+    }
+
+    const classesLeftToAttend = 
+        classesTotal !== null && classesAttended !== null && classesTotal > classesAttended
+            ? classesTotal - classesAttended
+            : 0;
+
+    return (
+        <>
+            <div className="mainpage">
+                <div className="container">
+                    <div className="card">
+                        <h1>Calculator</h1>
+                        <p>Calculate your attendance percentage with ease!</p>
+                        <div className="formcontainer">
+                            <form className="form" onSubmit={handleSubmit}>
+                                <input type="number" placeholder="Classes Total so-far" />
+                                <input type="number" placeholder="Classes Attended so-far" />
+                                <input type="number" placeholder="Total classes for the semester" />
+                                <button type="submit">Calculate</button>
+                            </form>
+                        </div>
+                        <h2>{percentage !== null ? `Attendance: ${percentage}%` : ""}</h2>
+                        <h2>{classesAttended}</h2>
+                        <h2>Classes left to attend: {classesLeftToAttend}</h2>
+                    </div>
+                </div>
+            </div>
+        </>
+    );
+}
+
 export default CalcPage;
